19eab9f5ce28683848ff703ff431c5d9695ed1c5
Karol Lisiecki <klisiecki@naviexpert.pl>
2019-12-15 22:43:50 +0100

    home thermostat - working 2xDS18B20

--- home_thermostat/home_thermostat.ino
+++ home_thermostat/home_thermostat.ino
@@ -17,31 +17,39 @@ int adc_key_in  = 0;
 #define btnSELECT 4
 #define btnNONE   5
 
+#define DEBUG 0
+#define DEBUG_KEYS 0
+
 struct HEATING_AREA {
   const char areaName[10];
   float currentTemp;
-  float setTemp;
+  float setTempOffset;
   int pin;
+  DeviceAddress deviceAddress;
 } areas[] =
 {
-  {"salon    ", -1, 21.5, A1},
-  {"kuchnia  ", -1, 22,   A2},
-  {"sypialnia", -1, 21,   A3},
-  {"pokoj    ", -1, 20.5, A4},
-  {"lazienka ", -1, 20.5, 0},
-  {"korytarz1", -1, 20.5, 1},
-  {"korytarz2", -1, 20.5, 11},
-  {"garderoba", -1, 20.5, 12},
+  {"salon    ", -1, 0, A1, { 0x28, 0xAA, 0x67, 0xB3, 0x53, 0x14, 0x01, 0xC2 } },
+  {"kuchnia  ", -1, 0, A2},
+  {"sypialnia", -1, 0, A3  },
+  {"pokoj    ", -1, 0, A4},
+  {"lazienka ", -1, 0, A5},
+  {"korytarz1", -1, 0, 3},
+  {"korytarz2", -1, 0, 11, { 0x28, 0xAA, 0x38, 0xD2, 0x53, 0x14, 0x01, 0x27 }},
+//  {"garderoba", -1, 0, 12},
 };
 
 const byte areasCount = (sizeof(areas) / sizeof(HEATING_AREA));
 
-int selectedArea = -1;
+float setTempBase = 24.0;
+int selectedArea = 0;
 byte lastPressed = -1;
+long lastRefreshTime;
+static long REFRESH_INTERVAL = 10000;
+static float TEMP_STEP = 0.1;
 
 int read_LCD_buttons() {
   adc_key_in = analogRead(0);
-  //Serial.println(adc_key_in);
+  if (DEBUG_KEYS) Serial.println(adc_key_in);
   if (adc_key_in > 1000) return btnNONE;
   if (adc_key_in < 50)   return btnRIGHT;
   if (adc_key_in < 150)  return btnUP;
@@ -52,31 +60,43 @@ int read_LCD_buttons() {
 }
 
 void setup() {
-  //Serial.begin(9600);
+  if (DEBUG || DEBUG_KEYS) {
+      Serial.begin(9600);
+  }
+  sensors.begin();
   lcd.begin(16, 2);
   for (int i = 0; i < areasCount; i++) {
     pinMode(areas[i].pin, OUTPUT);
   }
-  refreshScreen();
   updatePinsState();
+  refreshScreen();
 }
 
 void loop() {
   lcdKey = read_LCD_buttons();
-  if (lastPressed != lcdKey) {
+  if (lastPressed != lcdKey || lcdKey == btnRIGHT || lcdKey == btnLEFT) {
+    if (DEBUG) {
+      Serial.println(lcdKey);
+    }
     keyPressed(lcdKey);
     refreshScreen();
-    updatePinsState();
   }
   lastPressed = lcdKey;
+  if (millis() - lastRefreshTime > REFRESH_INTERVAL) {
+    updatePinsState();
+    refreshScreen();
+  }
+  delay(100);
 }
 
 void keyPressed (int lcdKey) {
   switch (lcdKey) {
     case btnRIGHT: {
+        tempUp();
         break;
       }
     case btnLEFT: {
+        tempDown();   
         break;
       }
     case btnUP: {
@@ -105,26 +125,60 @@ void keyPressed (int lcdKey) {
   }
 }
 
+void tempUp() {
+  if (selectedArea == -1) {
+    setTempBase = setTempBase + TEMP_STEP;
+  }
+  if (selectedArea >= 0) {
+    areas[selectedArea].setTempOffset = areas[selectedArea].setTempOffset + TEMP_STEP;
+  }
+}
+
+void tempDown() {
+  if (selectedArea == -1) {
+    setTempBase = setTempBase - TEMP_STEP;
+  }
+  if (selectedArea >= 0) {
+    areas[selectedArea].setTempOffset = areas[selectedArea].setTempOffset - TEMP_STEP;
+  }
+}
+
+float getTempSet(int areaIndex) {
+  return setTempBase + areas[areaIndex].setTempOffset; 
+}
+
 void refreshScreen() {
   lcd.clear();
   lcd.setCursor(0, 0);
-  lcd.print(">");
-  lcd.setCursor(1, 0);
   if (selectedArea >= 0) {
-    lcd.print(areas[selectedArea].areaName);
+    HEATING_AREA area = areas[selectedArea];
+    lcd.print(area.areaName);
+    lcd.print(" " + String(area.currentTemp, 1));
+    lcd.setCursor(0, 1);
+    lcd.print(":" + String(setTempBase, 1) + getSign(area.setTempOffset) + String(area.setTempOffset, 1) +"=" + String(getTempSet(selectedArea), 1));
   } else {
-    lcd.write("test");
+    lcd.print("stan: TODO" );
+    lcd.setCursor(0, 1);
+    lcd.print("Bazowa: " + String(setTempBase, 1));
   }
+  lastRefreshTime = millis();
+}
 
+String getSign(float offset) {
+  if (offset >= 0) return "+";
+  return "";
 }
 
 void updatePinsState() {
+  if (DEBUG) Serial.println("Updating temps");
+  sensors.requestTemperatures();
   for (int i = 0; i < areasCount; i++) {
-    if (i == selectedArea) {
+    areas[i].currentTemp = sensors.getTempC(areas[i].deviceAddress);
+    if (DEBUG) Serial.println("Temp for " + ((String) areas[i].areaName) + " is " + areas[i].currentTemp);
+    if (areas[i].currentTemp > 0 && areas[i].currentTemp < getTempSet(i)) {
       digitalWrite(areas[i].pin, HIGH);
     } else {
       digitalWrite(areas[i].pin, LOW);
     }
   }
-
 }
